afc1f90354ea04f9f21cca65acb81e546b9e4aa3
Amna Arooj <140439318+Amna541@users.noreply.github.com>
2023-08-01 22:52:45 +0500

    Update tag.py

--- tag.py
+++ tag.py
@@ -1,5 +1,5 @@
 from nlp_pos_tagger.pos_tagger import pos_tag_text
 
-text = "MY name is Amna Arooj. I work in Disrutive AI as an internee."
+text = "Enter any text here."
 tags = pos_tag_text(text)
 print(tags)
