6f1b42625c16575027101939a74f76f1cefb5a76
Amna Arooj <140439318+Amna541@users.noreply.github.com>
2023-08-02 00:02:36 +0500

    Update tag.py

--- tag.py
+++ tag.py
@@ -1,5 +1,7 @@
 from nlp_pos_tagger.pos_tagger import pos_tag_text
 
-text = "Enter any text here."
+text = "Write any text here.🙂"
 tags = pos_tag_text(text)
-print(tags)
+
+for word, tag, full_tag in tags:
+    print(f"Word: {word}, POS Tag: {tag}, Full POS: {full_tag}")
